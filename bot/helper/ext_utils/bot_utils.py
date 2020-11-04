ef245631b02fc953ca360992eb21855193d932eb
theaterplus <70135872+theaterplus@users.noreply.github.com>
2020-11-04 22:50:57 +0530

    Update bot_utils.py

--- bot/helper/ext_utils/bot_utils.py
+++ bot/helper/ext_utils/bot_utils.py
@@ -105,7 +105,7 @@ def get_readable_message():
                 except:
                     pass
             if download.status() == MirrorStatus.STATUS_DOWNLOADING:
-                msg += f"\n<code>/cancel2 {download.gid()}</code>"
+                msg += f"\n<code>/cancel1 {download.gid()}</code>"
             msg += "\n\n"
         return msg
 
